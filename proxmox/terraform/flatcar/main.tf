3b2c5a900ce08aab07708fcd4ebc683028ef61fa
Chloe Fitzgerald <chloefitzgerald271@gmail.com>
2025-08-03 19:30:28 +0000

    edit

--- proxmox/terraform/flatcar/main.tf
+++ proxmox/terraform/flatcar/main.tf
@@ -19,11 +19,11 @@ terraform {
 provider "proxmox" {
   pm_api_url      = var.pm_api_url
   pm_tls_insecure = true
-
+  
   pm_user             = var.pm_user
   pm_password         = var.pm_password
-  pm_api_token_id     = var.pm_api_token_id
-  pm_api_token_secret = var.pm_api_token_secret
+  pm_api_token_id     = var.token_id
+  pm_api_token_secret = var.token_secret
 }
 
 
