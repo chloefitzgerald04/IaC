5274c90d44f75038bbd827b2ed8244b675abb4bc
Chloe Fitzgerald <chloefitzgerald271@gmail.com>
2025-08-03 19:22:36 +0000

    edit

--- proxmox/terraform/flatcar/vars.tf
+++ proxmox/terraform/flatcar/vars.tf
@@ -76,7 +76,7 @@ variable "os_type_name" {
 variable "pm_api_url" {
   description = "The FQDN and path to the API of the proxmox server e.g. https://example.com:8006/api2/json"
   type        = string
-  default     = " https://10.0.0.102:8006/api2/json"
+  default     = "https://10.0.0.102:8006/api2/json"
 }
 
 variable "pm_api_token_id" {
